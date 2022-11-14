71b4b781ce03a067b259270fba2c69c92ddb250a
Achyut Timsina <actimsina@gmail.com>
2022-11-14 17:42:54 +0545

    basic express app

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require('express')
 const logger = require('./logger')
 
-logger.log("Some interesting event.")
+const app = express()
+
+app.use((req, res, next) => {
+    logger.log(req.path)
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running at port 3000')
+})
