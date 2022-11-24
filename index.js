447b5083236c2615c3fa16c96ed2b9ac281c860e
Achyut Timsina <actimsina@gmail.com>
2022-11-24 19:12:11 +0545

    express basics

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express')
+const path = require('path')
 const logger = require('./logger')
 
 const app = express()
 
 app.use((req, res, next) => {
-    logger.log(req.path)
+    logger.log(`${req.method}\t${req.headers.origin}\t${req.path}`)
+    console.log(`${req.method} ${req.path}`)
     next()
 })
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+// To accept form data
+app.use(express.urlencoded({ extended: false }))
+
+// To accept json data
+app.use(express.json())
+
+// To serve static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    // res.send('Hello world!')
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+// Error handling middleware
+
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).send(err.message)
 })
 
 app.listen(3000, () => {
